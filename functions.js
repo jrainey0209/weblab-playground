d11c3b5cb067fa359c7c8fe152175f062dff498f
jrainey0209 <buyer@John-Raineys-MacBook-Air.local>
2013-10-02 21:12:06 -0600

    Problem #3, Good Morning
    trying to use replace command, need help

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
-document.write('<h4>Word Cound Example</hr>');
-                
-var wordCount = function(text) {
+document.write('<h4>Word Count Example</h4>');
+
+var wordCount = function (text) {
   // \W matches non-word characters, match one or more
   var tokens = text.split(/\W+/);
   // account for empty elements at the start or end of the list
@@ -13,3 +13,5 @@ var wordCount = function(text) {
   }
   return count;
 }
+
+document.write('test 1: ' + wordCount(' one  two three'));
